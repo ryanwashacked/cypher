658543c4ce579010519688c954d5292dab644043
ryan-dervakos <ryan@efrontlearning.com>
2021-03-02 19:34:10 +0200

    Add delimiter to csv reader

--- read_questions_csv.py
+++ read_questions_csv.py
@@ -4,7 +4,7 @@ questions = []
 answers = []
 
 with open('csv/questions_answers.csv', newline='') as f:
-    reader = csv.reader(f)
+    reader = csv.reader(f, delimiter=';')
     for row in reader:
         questions.append(row[0])
         answers.append(row[1])
