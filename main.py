e3e0a7a0f655db5fb12403c19ed4ecd8b5201b1f
ryan-dervakos <ryan@efrontlearning.com>
2021-03-02 23:35:25 +0200

    Related questions

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ import numpy as np
 bc = BertClient(ip='34.107.68.82', check_length=False)
 
 q_embs = bc.encode(questions)
-
+topk = 5
 
 def answer_query(query):
     emb = bc.encode([query, "asfg"])[0].reshape((1, -1))
     similarities = [cosine_similarity(emb, x.reshape((1, -1))) for x in q_embs]
     most_similar = np.argmax(similarities)
+    results = []
+    score = np.sum(emb * q_embs, axis=1) / np.linalg.norm(q_embs, axis=1)
+    topk_idx = np.argsort(score)[::-1][:topk]
+    for idx in topk_idx:
+        # print('> %s\t%s' % (score[idx], questions[idx]))
+        results.append([questions[idx], answers[idx]])
 
-    return [questions[most_similar], answers[most_similar]]
+    return results
