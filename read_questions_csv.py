07ae19a8bc1a32cc75f2b8f6ff3842a641b11bfd
ryan-dervakos <ryan@efrontlearning.com>
2021-03-04 18:32:20 +0200

    Replace CSV with TSV

--- read_questions_csv.py
+++ read_questions_csv.py
@@ -3,8 +3,8 @@ import csv
 questions = []
 answers = []
 
-with open('csv/questions_answers.csv', newline='') as f:
-    reader = csv.reader(f, delimiter=';')
+with open('csv/questions_answers.tsv', newline='') as f:
+    reader = csv.reader(f, delimiter='\t')
     for row in reader:
         questions.append(row[0])
         answers.append(row[1])
