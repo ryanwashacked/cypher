0a4a322e246381e1573f0d6f1e287eac17b04b5a
ryan-dervakos <ryan@efrontlearning.com>
2021-03-04 16:09:01 +0200

    Add simple sqlite db and basic auth via JWT

---
+++ bert_functions.py
@@ -0,0 +1,22 @@
+from bert_serving.client import BertClient
+from read_questions_csv import questions, answers
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+bc = BertClient(ip='34.107.68.82', check_length=False)
+
+q_embs = bc.encode(questions)
+topk = 5
+
+def answer_query(query):
+    emb = bc.encode([query, "asfg"])[0].reshape((1, -1))
+    similarities = [cosine_similarity(emb, x.reshape((1, -1))) for x in q_embs]
+    most_similar = np.argmax(similarities)
+    results = []
+    score = np.sum(emb * q_embs, axis=1) / np.linalg.norm(q_embs, axis=1)
+    topk_idx = np.argsort(score)[::-1][:topk]
+    for idx in topk_idx:
+        # print('> %s\t%s' % (score[idx], questions[idx]))
+        results.append([questions[idx], answers[idx]])
+
+    return results
--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,34 @@
-from flask import Flask, escape, request
-from main import answer_query
+from flask import request
+from bert_functions import answer_query
 from flask_cors import CORS
 from flask import jsonify
+from flask_jwt import JWT, jwt_required
+from werkzeug.security import safe_str_cmp
+from user import User
+from main import app
 
 
-app = Flask(__name__)
+def authenticate(username, password):
+    user = User.query.filter(User.username == username).first()
+    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+
+def identity(payload):
+    return User.query.filter(User.id == payload['identity']).first()
+
+
+app.debug = True
+app.config['SECRET_KEY'] = 'super-secret'
+jwt = JWT(app, authenticate, identity)
 CORS(app)
+
+if __name__ == "__main__":
+    app.run()
 
 
 @app.route('/')
+@jwt_required()
 def get_answer():
     question = request.args.get('question')
     results = answer_query(question)
